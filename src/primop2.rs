2902de03c58fc05be36159369b92ab19162cb65e
Aaron Pribadi <aaron.pribadi@gmail.com>
2025-10-16 21:05:39 -0700

    z

--- src/primop2.rs
+++ src/primop2.rs
@@ -3,7 +3,7 @@ pub enum PrimOp2 {
   AddI64,
 }
 
-impl Op2 {
+impl PrimOp2 {
   pub fn as_str(self) -> &'static str {
     match self {
       Self::AddI64 => "add.i64",
@@ -11,7 +11,7 @@ impl Op2 {
   }
 }
 
-impl std::fmt::Display for Op2 {
+impl std::fmt::Display for PrimOp2 {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "{}", self.as_str())
   }
