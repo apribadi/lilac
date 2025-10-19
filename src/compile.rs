3a24285aaeba2ec41c3668acabb3dd3b128b91a6
Aaron Pribadi <aaron.pribadi@gmail.com>
2025-10-19 10:36:31 -0700

    z

--- src/compile.rs
+++ src/compile.rs
@@ -5,6 +5,10 @@ use crate::uir::Inst;
 struct Code(Vec<Inst>);
 
 impl Code {
+  fn new() -> Self {
+    Self(Vec::new())
+  }
+
   fn put(&mut self, inst: Inst) -> u32 {
     let n = self.0.len();
     self.0.push(inst);
@@ -21,6 +25,12 @@ struct Env {
 }
 
 impl Env {
+  fn new() -> Self {
+    Self {
+      args: Vec::new(),
+    }
+  }
+
   fn put_arg(&mut self, x: u32) {
     self.args.push(x)
   }
@@ -31,8 +41,8 @@ impl Env {
 }
 
 pub fn compile<'a>(x: Expr<'a>) -> Vec<Inst> {
-  let mut t = Env { args: Vec::new(), };
-  let mut o = Code(Vec::new());
+  let mut t = Env::new();
+  let mut o = Code::new();
 
   compile_expr_tail(&mut t, &mut o, x);
   return o.0;
@@ -197,89 +207,3 @@ fn compile_expr_tail<'a>(t: &mut Env, o: &mut Code, x: Expr<'a>) {
     }
   }
 }
-
-/*
-struct Env {
-  code: Vec<Inst>,
-  args: Vec<u32>,
-}
-
-impl Env {
-  fn emit(&mut self, inst: Inst) -> u32 {
-    let n = self.code.len();
-    self.code.push(inst);
-    return n as u32;
-  }
-
-  fn edit(&mut self, index: u32, inst: Inst) {
-    self.code[index as usize] = inst;
-  }
-}
-
-pub fn compile<'a>(x: Expr<'a>) -> Vec<Inst> {
-  let mut env =
-    Env {
-      code: Vec::new(),
-      args: Vec::new(),
-    };
-
-  compile_expr_tail(&mut env, x);
-  env.code
-}
-
-fn compile_expr_tail<'a>(env: &mut Env, x: Expr<'a>) {
-  match x {
-    Expr::And(&(x, y)) => {
-      let x = compile_expr(env, x);
-      let i = env.emit(Inst::Undefined); // Cond(x, a, b)
-      let a = env.emit(Inst::Label);
-      let f = env.emit(Inst::ConstBool(false));
-      let _ = env.emit(Inst::Put(f));
-      let _ = env.emit(Inst::Ret);
-      let b = env.emit(Inst::Label);
-      compile_expr_tail(env, y);
-      env.edit(i, Inst::Cond(x, a, b));
-    }
-    Expr::Call(&(f, xs)) => {
-      let f = compile_expr(env, f);
-      let mut ys = Vec::with_capacity(xs.len());
-      for &x in xs.iter() { ys.push(compile_expr(env, x)); }
-      for &y in ys.iter() { let _ = env.emit(Inst::Put(y)); }
-      let _ = env.emit(Inst::CallTail(f));
-    }
-    Expr::Or(&(x, y)) => {
-      let x = compile_expr(env, x);
-      let i = env.emit(Inst::Undefined); // Cond(x, a, b)
-      let a = env.emit(Inst::Label);
-      compile_expr_tail(env, y);
-      let b = env.emit(Inst::Label);
-      let t = env.emit(Inst::ConstBool(true));
-      let _ = env.emit(Inst::Put(t));
-      let _ = env.emit(Inst::Ret);
-      env.edit(i, Inst::Cond(x, a, b));
-    }
-    Expr::Ternary(&(p, x, y)) => {
-      let p = compile_expr(env, p);
-      let i = env.emit(Inst::Undefined); // Cond(p, a, b)
-      let a = env.emit(Inst::Label);
-      compile_expr_tail(env, x);
-      let b = env.emit(Inst::Label);
-      compile_expr_tail(env, y);
-      env.edit(i, Inst::Cond(p, a, b));
-    }
-    x @ (
-      | Expr::Field(_)
-      | Expr::Index(_)
-      | Expr::Int(_)
-      | Expr::Op1(_)
-      | Expr::Op2(_)
-      | Expr::Undefined
-      | Expr::Variable(_)
-    ) => {
-      let x = compile_expr(env, x);
-      let _ = env.emit(Inst::Put(x));
-      let _ = env.emit(Inst::Ret);
-    }
-  }
-}
-*/
